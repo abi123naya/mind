c32a6d3f29b43dc8da10503117033193a1e44c07
abi123naya <connect2abinaya@gmail.com>
2016-07-08 11:05:49 +0530

    Update and rename README.md to README.java
    finish

---
+++ README.java
@@ -0,0 +1,16 @@
+# mind
+import.java.util.*;
+class VowelConso
+{ public static void main(String[] args)
+  { char x;
+  Scanner s=new Scanner(System.in);
+  x=s.nextChar();
+  }
+void dec()
+{ if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+{System.out.println("Vowel"); }
+else
+{System.out.println("Consonent");} 
+
+
+}
